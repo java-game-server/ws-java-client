360a8787ebab89f192739c150c59b7635757d219
Raikish <albert.montagut.casero@gmail.com>
2018-04-04 17:16:36 +0200

    Implementing dao

--- src/main/java/com/apporelbotna/gameserver/persistencewsclient/DAO.java
+++ src/main/java/com/apporelbotna/gameserver/persistencewsclient/DAO.java
@@ -1,8 +1,10 @@
 package com.apporelbotna.gameserver.persistencewsclient;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +12,8 @@ import com.apporelbotna.gameserver.stubs.Game;
 import com.apporelbotna.gameserver.stubs.Token;
 import com.apporelbotna.gameserver.stubs.User;
 import com.apporelbotna.gameserver.stubs.UserWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 //TODO los memtodos devuelven un HttpStatus que es el code de segun si ha ido bien, mal, etc.
 //esto se podria hacer alguna clase que controlase los codigos que se mostraran en la applicacion
@@ -24,55 +28,76 @@ public class DAO
 
 	}
 
-	public User getUserInformation(User user)
+	public User getUserInformation(User user, Token token)
 	{
 		// TODO implement
 		RestTemplate restTemplate = new RestTemplate();
 
-		ResponseEntity<User> userResponse = restTemplate.getForEntity(SERVER_URL + "" + user.getId(), User.class);
+		ResponseEntity<User> userResponse = restTemplate
+				.getForEntity(SERVER_URL + "" + user.getId(), User.class);
 
 		return userResponse.getBody();
 	}
 
-	//METHOD CheckUserToken
-
-	public String createUser(UserWrapper userWrapper)
-	{
-		RestTemplate restTemplate = new RestTemplate();
-
-		ResponseEntity<String> response = restTemplate.postForEntity(SERVER_URL + "user",
-				userWrapper, String.class);
-		return response.getBody();
-	}
-
-	public Token login(UserWrapper userWrapper)
-	{
-		RestTemplate restTemplate = new RestTemplate();
-
-		Token token = restTemplate.postForObject(SERVER_URL + "/login", userWrapper, Token.class);
-
-		return token;
-	}
-
+	// METHOD CheckUserToken
 	// create metodo que verifique si el user tiene un token
+	public boolean isUserLoggeable(UserWrapper userWrapper)
+	{
+
+		return true;
+	}
+
+	public UserWrapper login(String email, String password)
+	{
+
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.getForObject(SERVER_URL + "/login/" + email + "/" + password,
+				UserWrapper.class);
+	}
 
 	// saca todos los juegos de un usuario
 	public List<Game> findAllGamesByUser(User user)
 	{
+		String userEmail = user.getId();
+
 		RestTemplate restTemplate = new RestTemplate();
 
-		String userEmail = user.getId();
-		ResponseEntity<List> response = restTemplate
-				.getForEntity(SERVER_URL + "/user/game/" + userEmail, List.class);
-		ArrayList<String> a = (ArrayList<String>) response.getBody();
-		for (String iterable_element : a)
+		ResponseEntity<List<Game>> responseWS = restTemplate.exchange(
+				SERVER_URL + "/user/game/" + userEmail, HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Game>>()
+				{
+				});
+
+		return responseWS.getBody();
+	}
+
+	public String createUser(User user, String password)
+	{
+		RestTemplate restTemplate = new RestTemplate();
+
+		ObjectMapper mapper = new ObjectMapper();
+		try
 		{
-			System.out.println(iterable_element);
+//			System.out.println(mapper.writeValueAsString(user));
+//			System.out.println(mapper.writeValueAsString(password));
+
+			// Create the node factory that gives us nodes.
+			Map<String, Object> map = mapper.readValue(json, new TypeReference<Map<String,Object>>(){});
+
+
+
+		} catch (JsonProcessingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		// TODO MAKE IT WORKS.
-		return null;
+		// objMapper.
 
+//		ResponseEntity<String> response = restTemplate.postForEntity(SERVER_URL + "user",
+//				userWrapper, null);
+
+		return null;
 	}
 
 	public static void main(String[] args)
@@ -80,31 +105,30 @@ public class DAO
 		DAO dao = new DAO();
 
 		// Creating User
-		/*
-		 * User user = new User("janJanitoJwzdf3sanbo@Tronchaco.com", "Jan");
-		 * UserWrapper userWrapper = new UserWrapper(user, "1234"); DAO dao = new DAO();
-		 * System.out.println(dao.createUser(userWrapper));
-		 */
+		 User user = new User("janJanitoJwzdf3sssanbo@Tronchaco.com", "Jan");
+		 String password = "1234";
+
+		 System.out.println(dao.createUser(user, password));
 
 		// Testing log in
-		/*
-		 * String email = "jan@jan.com"; String password = "1234"; UserWrapper
-		 * userWrapper = new UserWrapper(new User(email, "das"), "1234");
-		 *
-		 * Token token = dao.login(userWrapper);
-		 */
+		//
+		// String email = "jan@jan.com";
+		// String password = "1234";
+		// Token token = dao.login(email, password);
+		// System.out.println(token);
 
 		// Find all games by username
-		/*
-		List<Game> games = dao.findAllGamesByUser(new User("jan@jan.com", "pp"));
-		for (Game game : games)
-		{
-			System.out.println(game);
-		}*/
+		// List<Game> games = dao.findAllGamesByUser(new User("jan@jan.com", "pp"));
+		// for (Game game : games)
+		// {
+		// System.out.println(game);
+		// }
 
-		//TEST USER INFORMATION
-		User userInformatiton = dao.getUserInformation(new User("jan@jan.com","jan"));
-		System.out.println(userInformatiton);
+		// TEST USER INFORMATION
+		/*
+		 * User userInformatiton = dao.getUserInformation(new
+		 * User("jan@jan.com","jan")); System.out.println(userInformatiton);
+		 */
 	}
 
 }
