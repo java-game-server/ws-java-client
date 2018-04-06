8c34994f02de2c819360d0070b5d249ec7870a7d
Raikish <albert.montagut.casero@gmail.com>
2018-04-06 04:41:38 +0200

    changing url server

--- src/main/java/com/apporelbotna/gameserver/persistencewsclient/GameDAO.java
+++ src/main/java/com/apporelbotna/gameserver/persistencewsclient/GameDAO.java
@@ -18,7 +18,7 @@ import com.apporelbotna.gameserver.stubs.UserWrapper;
 
 public class GameDAO
 {
-	public static final String SERVER_URL = "http://localhost:8082/";
+    public static final String SERVER_URL = "http://172.16.2.94:8082/";
 	RestTemplate restTemplate = new RestTemplate();
 
 	public GameDAO()
