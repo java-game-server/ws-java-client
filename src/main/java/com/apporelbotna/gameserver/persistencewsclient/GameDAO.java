0f908b8d43852a40378f35bbab588b1de29b4a09
Raikish <albert.montagut.casero@gmail.com>
2018-05-17 01:08:28 +0200

    see all users store game now it's a post method

--- src/main/java/com/apporelbotna/gameserver/persistencewsclient/GameDAO.java
+++ src/main/java/com/apporelbotna/gameserver/persistencewsclient/GameDAO.java
@@ -20,186 +20,169 @@ import com.apporelbotna.gameserver.stubs.UserWrapper;
 
 public class GameDAO
 {
-    // Stucky server
-    public static final String SERVER_URL = "http://172.16.2.94:8082/";
-   // public static final String SERVER_URL = "http://localhost:8082/";
+	// Stucky server
+	public static final String SERVER_URL = "http://172.16.2.94:8082/";
+	//public static final String SERVER_URL = "http://localhost:8082/";
 
-    RestTemplate restTemplate = new RestTemplate();
+	RestTemplate restTemplate = new RestTemplate();
 
-    public GameDAO()
-    {
-	// Empty constructor
-    }
-
-    public boolean finishMatch(Match... matches)
-    {
-	for ( Match match : matches )
+	public GameDAO()
 	{
-	    ResponseEntity< ? > response = restTemplate.postForEntity( SERVER_URL + "/match", match, null );
-	    if ( !response.getStatusCode().equals( HttpStatus.CREATED ) )
-	    {
-		return false;
-	    }
+		// Empty constructor
 	}
-	return true;
-    }
 
-    public boolean isUserLoggeable(String email, String tokenString)
-    {
-	UserWrapper wrapper = new UserWrapper( new User( email ), new Token( tokenString ) );
+	public boolean finishMatch(Match... matches)
+	{
+		for (Match match : matches)
+		{
+			ResponseEntity<?> response = restTemplate.postForEntity(SERVER_URL + "/match", match, null);
+			if (!response.getStatusCode().equals(HttpStatus.CREATED))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
-	ResponseEntity< ? > response = restTemplate.postForEntity( SERVER_URL + "/auth", wrapper, null );
-	return( response.getStatusCode().equals( HttpStatus.OK ) );
-    }
+	public boolean isUserLoggeable(String email, String tokenString)
+	{
+		UserWrapper wrapper = new UserWrapper(new User(email), new Token(tokenString));
 
-    public User validateUser(String email, String tokenString)
-    {
-	return isUserLoggeable( email, tokenString ) ? getUserInformation( email ) : null;
-    }
+		ResponseEntity<?> response = restTemplate.postForEntity(SERVER_URL + "/auth", wrapper, null);
+		return (response.getStatusCode().equals(HttpStatus.OK));
+	}
 
-    public Token login(String email, String password)
-    {
-	return restTemplate.getForObject( SERVER_URL + "/login/" + email + "/" + password, Token.class );
-    }
+	public User validateUser(String email, String tokenString)
+	{
+		return isUserLoggeable(email, tokenString) ? getUserInformation(email) : null;
+	}
 
-    // saca todos los juegos de un usuario
-    public List< Game > findAllGamesByUser(User user)
-    {
-	String userEmail = user.getId();
-	ResponseEntity< List< Game > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/user/game/" + userEmail,
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< Game > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	public Token login(String email, String password)
+	{
+		return restTemplate.getForObject(SERVER_URL + "/login/" + email + "/" + password, Token.class);
+	}
 
-    public boolean createUser(User user, String password)
-    {
-	RegisterUser userToRegister = new RegisterUser( user, password );
+	// saca todos los juegos de un usuario
+	public List<Game> findAllGamesByUser(User user)
+	{
+		String userEmail = user.getId();
+		ResponseEntity<List<Game>> responseWS = restTemplate.exchange(SERVER_URL + "/user/game/" + userEmail,
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Game>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-	ResponseEntity< ? > response = restTemplate.postForEntity( SERVER_URL + "/user/", userToRegister, null );
+	// saca todos los juegos de un usuario
+	public List<User> findAllUser()
+	{
+		ResponseEntity<List<User>> responseWS = restTemplate.exchange(SERVER_URL + "/user/",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-	return response.getStatusCode().equals( HttpStatus.CREATED );
-    }
+	public boolean createUser(User user, String password)
+	{
+		RegisterUser userToRegister = new RegisterUser(user, password);
 
-    public boolean createGame(Game game)
-    {
-	ResponseEntity< ? > response = restTemplate.postForEntity( SERVER_URL + "/game/", game, null );
+		ResponseEntity<?> response = restTemplate.postForEntity(SERVER_URL + "/user/", userToRegister, null);
 
-	return response.getStatusCode().equals( HttpStatus.CREATED );
-    }
+		return response.getStatusCode().equals(HttpStatus.CREATED);
+	}
 
-    public void storeGameToUser(User user, int idGame)
-    {
-	Map< String , String > params = new HashMap<>();
-	params.put( "idGame", String.valueOf( idGame ) );
+	public boolean createGame(Game game)
+	{
+		ResponseEntity<?> response = restTemplate.postForEntity(SERVER_URL + "/game/", game, null);
 
-	restTemplate.put( SERVER_URL + "/game/{idGame}", user, params );
-    }
+		return response.getStatusCode().equals(HttpStatus.CREATED);
+	}
 
-    public void updateUserBasicInformatio(User user)
-    {
-	Map< String , String > params = new HashMap<>();
+	public boolean storeGameToUser(User user, int idGame)
+	{
+		Map<String, String> params = new HashMap<>();
+		params.put("idGame", String.valueOf(idGame));
 
-	restTemplate.put( SERVER_URL + "/user", user, params );
-    }
+		ResponseEntity<?> response = restTemplate.postForEntity(SERVER_URL + "/game/{idGame}", user, null, params);
+		return response.getStatusCode().equals(HttpStatus.OK);
+	}
 
-    public User getUserInformation(String email)
-    {
-	return restTemplate.getForObject( SERVER_URL + "/user/" + email, User.class );
-    }
+	public void updateUserBasicInformatio(User user)
+	{
+		Map<String, String> params = new HashMap<>();
 
-    /**
-     *
-     * @param email
-     * @param gameID
-     * @return time in miliseconds if player never played to this game result will
-     *         be <b>0.0</b>
-     */
-    public float gameTimePlayedByGame(String email, int gameID)
-    {
-	return restTemplate.getForObject( SERVER_URL + "/user/" + email + "/game/" + gameID + "/time/", Float.class );
-    }
+		restTemplate.put(SERVER_URL + "/user", user, params);
+	}
 
-    public List< RankingPointsTO > getRankingPointsByGameAndUser(int gameId)
-    {
-	ResponseEntity< List< RankingPointsTO > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/ranking/" + gameId,
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< RankingPointsTO > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	public User getUserInformation(String email)
+	{
+		return restTemplate.getForObject(SERVER_URL + "/user/" + email, User.class);
+	}
 
-    public List< Game > getAllGames()
-    {
-	ResponseEntity< List< Game > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/game/",
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< Game > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	/**
+	 *
+	 * @param email
+	 * @param gameID
+	 * @return time in miliseconds if player never played to this game result
+	 *         will be <b>0.0</b>
+	 */
+	public float gameTimePlayedByGame(String email, int gameID)
+	{
+		return restTemplate.getForObject(SERVER_URL + "/user/" + email + "/game/" + gameID + "/time/", Float.class);
+	}
 
-    public List< String > getAllGenre()
-    {
-	ResponseEntity< List< String > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/genre/",
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< String > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	public List<RankingPointsTO> getRankingPointsByGameAndUser(int gameId)
+	{
+		ResponseEntity<List<RankingPointsTO>> responseWS = restTemplate.exchange(SERVER_URL + "/ranking/" + gameId,
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<RankingPointsTO>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-    public List< User > findUsersLikeName(String userInput)
-    {
-	ResponseEntity< List< User > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/user/" + userInput + "/name/",
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< User > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	public List<Game> getAllGames()
+	{
+		ResponseEntity<List<Game>> responseWS = restTemplate.exchange(SERVER_URL + "/game/", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Game>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-    public List< User > findUsersLikeEmail(String userInput)
-    {
-	ResponseEntity< List< User > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/user/" + userInput + "/email/",
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< User > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	public List<String> getAllGenre()
+	{
+		ResponseEntity<List<String>> responseWS = restTemplate.exchange(SERVER_URL + "/genre/", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<String>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-    public List< Game > findGamesLikeName(String userInput)
-    {
-	ResponseEntity< List< Game > > responseWS
-		= restTemplate.exchange( SERVER_URL + "/game/" + userInput + "/name/",
-			HttpMethod.GET,
-			null,
-			new ParameterizedTypeReference< List< Game > >()
-			{ //
-			} );
-	return responseWS.getBody();
-    }
+	public List<User> findUsersLikeName(String userInput)
+	{
+		ResponseEntity<List<User>> responseWS = restTemplate.exchange(SERVER_URL + "/user/" + userInput + "/name/",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-    public static void main( String[] args )
-    {
-	GameDAO dao = new GameDAO();
+	public List<User> findUsersLikeEmail(String userInput)
+	{
+		ResponseEntity<List<User>> responseWS = restTemplate.exchange(SERVER_URL + "/user/" + userInput + "/email/",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 
-	dao.storeGameToUser( new User( "jan@jan.com" ) , 6 );
-    }
+	public List<Game> findGamesLikeName(String userInput)
+	{
+		ResponseEntity<List<Game>> responseWS = restTemplate.exchange(SERVER_URL + "/game/" + userInput + "/name/",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Game>>()
+				{ //
+				});
+		return responseWS.getBody();
+	}
 }
